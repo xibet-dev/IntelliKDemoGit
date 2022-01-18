34741f219b2c006bd3a10408faa0db905760b9d6
xibet-dev <marco.scibetta.dev@gmail.com>
2022-01-18 10:55:31 +0100

    Add hello world message

--- src/org/launchcode/Main.java
+++ src/org/launchcode/Main.java
@@ -3,7 +3,8 @@ package org.launchcode;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-    //first change
+        // write your code here
+        //first chang
+        System.out.println("Hello world!");
     }
 }
